38f08f8b190432b017b5be4a1e6b0c75d925ada3
Sl4vP0weR <42337892+Sl4vP0weR@users.noreply.github.com>
2023-01-04 18:32:22 +0800

    Fix compatibility.

---
+++ code/Host.cs
@@ -0,0 +1,14 @@
+﻿internal static class Host
+{
+    public static string Name
+    {
+        get
+        {
+            if( IsServer )
+            {
+                return "Server";
+            }
+            return SteamId.ToString();
+        }
+    }
+}
